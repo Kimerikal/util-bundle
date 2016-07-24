47c33e1ebd3e8b5b52341a9d261632b732e3dae3
joseca <joseca@jsc>
2016-07-24 19:35:59 +0200

    Added generic form creation by annotation

---
+++ Annotations/FormData.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kimerikal\UtilBundle\Annotations;
+
+use Doctrine\Common\Annotations\Annotation;
+
+/**
+ * @Annotation
+ * @Target({"PROPERTY"})
+ */
+final class FormData extends Annotation {
+    public $type;
+    public $label;
+    public $col = 12;
+    public $placeholder = '';
+    public $required = false;
+    public $newLine = false;
+    public $order = 0;
+}
---
+++ Form/SimpleForm.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Kimerikal\UtilBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+class SimpleForm extends AbstractType {
+
+    private $trans;
+    private $class;
+
+    public function __construct($class, $trans) {
+        $this->class = $class;
+        $this->trans = $trans;
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $reader = new AnnotationReader();
+        $reflectionClass = new \ReflectionClass($this->class);
+        $props = $reflectionClass->getProperties();
+        foreach ($props as $p) {
+            $reflectionProperty = new \ReflectionProperty($this->class, $p->name);
+            $fd = $reader->getPropertyAnnotation($reflectionProperty, 'Kimerikal\\UtilBundle\\Annotations\\FormData');
+            if ($fd) {
+                $attrs = array(
+                    'class' => 'form-control' . ($fd->type == 'checkbox' ? ' make-switch' : ''),
+                    'placeholder' => $fd->placeholder,
+                    'nlal' => $fd->newLine,
+                    'bcol' => $fd->col
+                );
+                
+                if ($fd->type == 'decimal') {
+                    $fd->type = 'number';
+                    $attrs['step'] = 'any';
+                }
+
+                $builder->add($p->name, $fd->type, array(
+                    'label' => $fd->label,
+                    'required' => $fd->required,
+                    'attr' => $attrs
+                ));
+            }
+        }
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => $this->class
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return 'kimerikal_utilbundle_' . mb_strtolower(str_replace('\\', '_', $this->class));
+    }
+
+}
